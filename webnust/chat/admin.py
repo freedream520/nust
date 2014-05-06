da6771201851864187bb3290335709407c9697e9
MrGba2z <MrRegister@outlook.com>
2014-05-06 21:32:51 +0800

    add chatadmin in background

--- webnust/chat/admin.py
+++ webnust/chat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from chat.models import Article
 
-# Register your models here.
+
+class ChatAdmin(admin.ModelAdmin):
+	list_display = ('title', 'body', 'likes','pub_date')
+	fieldsets = [
+		(None, {'fields': ['title']}),
+		('Date information', {'fields': ['body'], 'classes': ['collapse']}),
+		(None, {'fields':['likes']}),
+		(None, {'fields':['pub_date']}),
+	]
+	list_filter = ['body']
+	search_fields = ['title']
+
+admin.site.register(Article, ChatAdmin)
