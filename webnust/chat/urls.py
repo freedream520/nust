d26588d97942a2e0573cc7985c7392bc3e9d0f88
MrGba2z <MrRegister@outlook.com>
2014-05-16 00:33:05 +0800

    delete useless lines

--- webnust/chat/urls.py
+++ webnust/chat/urls.py
@@ -7,19 +7,17 @@ from api import ArticleResource
 article_resource = ArticleResource()
 
 urlpatterns = patterns('',
-	#url(r'^$', views.IndexView.as_view(), name='index'),
-	#url(r'^vote/$', views.vote, name='vote'),
 	url(r'^all/$', views.articles),
 	url(r'^(?P<article_id>\d+)/$', views.article,),
-	#url(r'^language/(?P<language>[a-z\-]+)/$', 'article.views.language'),
 	url(r'^create/$', views.create),
 	url(r'^like/(?P<article_id>\d+)/$', views.like_article),
 	url(r'^add_comment/(?P<article_id>\d+)/$', views.add_comment),
 	url(r'^delete_comment/(?P<comment_id>\d+)/$', views.delete_comment),
 	url(r'^search/$', views.search_titles),
-	# url(r'^explore/$', views.search_group),
-	# url(r'^search/', include('haystack.urls')),
 	url(r'^api/', include(article_resource.urls)),
+	# url(r'^search/', include('haystack.urls')),
+	# url(r'^language/(?P<language>[a-z\-]+)/$', 'article.views.language'),
+
 )
 
 if settings.DEBUG:
